d84cfbd0c75a66e31bdc3ced7da88d6d1753af19
Michael Yeates <michaeljyeates@gmail.com>
2020-08-23 19:49:57 +0100

    Add Redeem page

---
+++ ui/aliensale/src/pages/Redeem.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="full-width column wrap justify-start content-center">
+    <div>
+      <div class="text-h1">Redeem Vouchers</div>
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'RedeemPage',
+  components: {},
+  computed: {
+    ...mapGetters({
+      getAccountName: 'ual/getAccountName'
+    })
+  }
+}
+</script>
