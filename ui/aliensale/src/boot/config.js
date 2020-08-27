3648a450d3e60c70334f007fb0f01b0d6534aea1
Michael Yeates <michaeljyeates@gmail.com>
2020-08-27 19:26:32 +0100

    Add collection name to config

--- ui/aliensale/src/boot/config.js
+++ ui/aliensale/src/boot/config.js
@@ -4,6 +4,7 @@ export default ({ Vue }) => {
     waxEndpoint: 'https://testnet.waxsweden.org',
     eosEndpoint: 'https://jungle3.eosdac.io',
     atomicEndpoint: 'https://test.wax.api.atomicassets.io',
+    collectionName: 'test.worlds',
     redeemContractEos: 'alienpromopk',
     redeemBurnEos: 'alienworlds1'
   }
