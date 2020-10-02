bb2a9d63786c8011997e8c9f8e517dbc43ba4cc6
Michael Yeates <michaeljyeates@gmail.com>
2020-10-02 22:34:12 +0100

    Store referrer in localstorage

--- ui/aliensale/src/boot/referrer.js
+++ ui/aliensale/src/boot/referrer.js
@@ -1,13 +1,23 @@
 
 export default ({ Vue, urlPath }) => {
-  const [, search] = urlPath.split('?')
   let referrerAccount = ''
-  if (typeof search !== 'undefined') {
-    const referrerPart = search.split('&').filter(s => s.substr(0, 2) === 'r=')
-    if (referrerPart.length) {
-      [, referrerAccount] = referrerPart[0].split('=')
+  if (localStorage) {
+    referrerAccount = localStorage.getItem('referrer')
+  }
+  if (!referrerAccount) {
+    const [, search] = urlPath.split('?')
+    if (typeof search !== 'undefined') {
+      const referrerPart = search.split('&').filter(s => s.substr(0, 2) === 'r=')
+      if (referrerPart.length) {
+        [, referrerAccount] = referrerPart[0].split('=')
+      }
     }
   }
+
+  if (localStorage && referrerAccount) {
+    localStorage.setItem('referrer', referrerAccount)
+  }
+
   console.log(`Referrer = ${referrerAccount}`)
   Vue.prototype.$referrer = referrerAccount
 }
