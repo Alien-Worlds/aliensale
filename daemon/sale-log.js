0d1fbf7e2b1fbfee319a26c22b19794c5edfc6bb
Michael Yeates <michaeljyeates@gmail.com>
2020-10-03 18:54:49 +0100

    Add tests for txid and country format

--- daemon/sale-log.js
+++ daemon/sale-log.js
@@ -19,9 +19,26 @@ fastify.register(require('fastify-cors'), {
 });
 
 fastify.post('/sale', async (request, reply) => {
-    const req = JSON.parse(request.body);
+    // console.log(request.body);
+    let req = request.body;
+    if (typeof request.body === 'string'){
+        req = JSON.parse(request.body);
+    }
+    // console.log(req);
     if (!req || !req.txId || !req.country) {
         throw new Error('You must supply txId and country as part of the post JSON');
+    }
+    req.txId = req.txId.toLowerCase();
+    // check txId is hex
+    const ret = /^[a-z0-9]{64}$/;
+    const hexTest = ret.test(req.txId);
+    // console.log(hexTest)
+    if (!hexTest){
+        throw new Error('Transaction ID is in the wrong format');
+    }
+    const rec = /^[A-Z]{2}$/;
+    if (!rec.test(req.country) || req.country.length !== 2){
+        throw new Error('Country in wrong format');
     }
     fs.writeFileSync(`receipts/${req.txId}.txt`, req.country);
     return { success: true };
