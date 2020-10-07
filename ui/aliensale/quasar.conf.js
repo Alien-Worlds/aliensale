8b9279ed71a5d01360385490b19be923b137884d
Michael Yeates <michaeljyeates@gmail.com>
2020-10-07 12:58:33 +0100

    Put mobile styles after main

--- ui/aliensale/quasar.conf.js
+++ ui/aliensale/quasar.conf.js
@@ -36,8 +36,8 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'mobile.sass',
-      'app.sass'
+      'app.sass',
+      'mobile.sass'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
