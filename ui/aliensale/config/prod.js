54d457ab7e1eee1d951b4dbdd69824333d671483
Michael Yeates <michaeljyeates@gmail.com>
2020-10-08 21:14:37 +0100

    Add chain ids to prod config

--- ui/aliensale/config/prod.js
+++ ui/aliensale/config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
     waxEndpoint: 'https://api.waxsweden.org',
+    waxChainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
     eosEndpoint: 'https://api.eossweden.org',
+    eosChainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     atomicEndpoint: 'https://wax.api.atomicassets.io',
     collectionName: 'alien.worlds',
     saleContract: 'sale.worlds',
