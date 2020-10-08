27949fc42c5f6167979bb895c3b28d4aa8186fd2
Michael Yeates <michaeljyeates@gmail.com>
2020-10-08 21:00:43 +0100

    Final production config

--- ui/aliensale/config/prod.js
+++ ui/aliensale/config/prod.js
@@ -1,22 +1,5 @@
 module.exports = {
-    waxEndpoint: 'https://wax.eosdac.io',
-    waxChainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-    eosEndpoint: 'https://api.eossweden.org',
-    eosChainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    atomicEndpoint: 'https://wax.api.atomicassets.io',
-    collectionName: '1jcjxbr5d3aj',
-    saleContract: '2y25yjltikdf',
-    openContract: 'rfwz2svl2m4k',
-    packContract: '1jcjxbr5d3aj',
-    redeemContractEos: 'alienpromopk',
-    redeemBurnEos: 'alienworlds1',
-    redeemServer: 'https://redeem.alienworlds.io',
-    saleServer: 'https://sale.alienworlds.io',
-    ipfsRoot: 'https://cloudflare-ipfs.com/ipfs/'
-}
-/*
-module.exports = {
-    waxEndpoint: 'https://wax.eosdac.io',
+    waxEndpoint: 'https://api.waxsweden.org',
     eosEndpoint: 'https://api.eossweden.org',
     atomicEndpoint: 'https://wax.api.atomicassets.io',
     collectionName: 'alien.worlds',
@@ -29,4 +12,3 @@ module.exports = {
     saleServer: 'https://sale.alienworlds.io',
     ipfsRoot: 'https://cloudflare-ipfs.com/ipfs/'
 }
-*/
